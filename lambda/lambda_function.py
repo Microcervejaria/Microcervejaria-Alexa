fb8f485abdb5789c1350d16beb417db5975a171c
João Saliba <joao_salibaa@hotmail.com>
2020-10-14 22:51:05 -0300

    Apagango HelpIntentHandler velho

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -55,23 +55,6 @@ class OlaMundoIntentHandler(AbstractRequestHandler):
         )
 
 
-class HelpIntentHandler(AbstractRequestHandler):
-    """Handler for Help Intent."""
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speak_output = "You can say hello to me! How can I help?"
-
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                .ask(speak_output)
-                .response
-        )
-
 
 class CancelOrStopIntentHandler(AbstractRequestHandler):
     """Single handler for Cancel and Stop Intent."""
