de387f860636db202182cfd6bb5a1498fac3f57f
João Saliba <joao_salibaa@hotmail.com>
2020-10-14 22:52:57 -0300

    Adicionando HelpIntentHandler novo

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -36,6 +36,24 @@ class LaunchRequestHandler(AbstractRequestHandler):
                 .response
         )
 
+class HelpIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return ask_utils.is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Você pode pedir para iniciar alguma receita, iniciar limpeza, listar as receitas, ou até mesmo detalhar alguma receitas  Podendo também indicar o tempo para fim de algum  processo, ou tempo para fim da limpeza"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
+        
+
 
 class OlaMundoIntentHandler(AbstractRequestHandler):
     """Handler for Hello World Intent."""
